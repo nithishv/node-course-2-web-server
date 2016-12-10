417f8ecf36a2fc12702cf3d73cc397224ffe756e
Nithish V <v.nithish@gmail.com>
2016-12-11 00:19:24 +0530

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -57,6 +58,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(80, () => {
-  console.log('Listening on localhost');
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
 });
