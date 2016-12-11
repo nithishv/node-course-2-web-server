afce42a75834c7e1648f4348ffff0493eac2dbef
Nithish V <v.nithish@gmail.com>
2016-12-11 09:12:15 +0530

    Added Projects Page

--- server.js
+++ server.js
@@ -52,6 +52,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
+    projectsPageData: 'My Portfolio'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     error: 'Error in the request'
